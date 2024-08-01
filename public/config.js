851e93d3065ff4b0b0a28dccf6bc932bc1705e59
alesierra <alesierraalta@gmail.com>
2024-08-01 18:19:12 -0400

    endpoints updt

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 // config.js
 window.configs = {
-  categoriasApiUrl: '/choreo-apis/isolamenunest/backnest/v1',
+  categoriasApiUrl: '/choreo-apis/isolamenunest/backnest/categoriasservice-a89/v1',
   comidasApiUrl: '/choreo-apis/isolamenunest/backnest/comidasbycategoriaservice-468/v1',
 };
