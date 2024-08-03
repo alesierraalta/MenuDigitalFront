219841a93a524892dbd29a123fb89a2ef4e78653
alesierra <alesierraalta@gmail.com>
2024-08-02 20:43:30 -0400

    services updt

--- src/services/categoriaService.js
+++ src/services/categoriaService.js
@@ -11,7 +11,12 @@ const api = axios.create({
 export const getCategorias = async () => {
   try {
     const response = await api.get('/');
-    return response.data;
+    console.log('API Response:', response.data); // Log the response
+    if (Array.isArray(response.data)) {
+      return response.data;
+    } else {
+      throw new Error('API response is not an array');
+    }
   } catch (error) {
     console.error('Error fetching categorias:', error.message);
     console.log('Error details:', error.response?.data || error);
