9f9babad65178899549c74462b6d7f05c45ffc3b
alesierra <alesierraalta@gmail.com>
2024-08-04 20:02:30 -0400

    reload comidas fix v0.1

--- src/services/comidaService.js
+++ src/services/comidaService.js
@@ -3,29 +3,29 @@ import { handleApiError } from '../utils/errorHandler';
 
 // Obtener la URL de la API desde las configuraciones globales
 const apiUrl = window?.configs?.apiUrl ? window.configs.apiUrl : "/";
-console.log('Using API URL:', apiUrl); // Log the base URL
+console.log('Using API URL:', apiUrl);
 
 const api = axios.create({
   baseURL: apiUrl,
 });
 
 export const getComidasPorCategoria = async (idCategoria) => {
-  const endpoint = `/api/comidas/categoria/${idCategoria}`; // Asegúrate de que el endpoint comience con una barra
+  const endpoint = `/api/comidas/categoria/${idCategoria}`;
   const requestUrl = `${apiUrl}${endpoint}`;
   console.log('Making API request to:', requestUrl);
 
   try {
     const response = await api.get(endpoint);
-    console.log('API Response Status:', response.status); // Log the response status
-    console.log('API Response Headers:', response.headers); // Log the response headers
-    console.log('API Response Data:', response.data); // Log the response data
+    console.log('API Response Status:', response.status);
+    console.log('API Response Headers:', response.headers);
+    console.log('API Response Data:', response.data);
 
     if (Array.isArray(response.data)) {
       console.log('Response is an array');
       return response.data;
     } else {
       console.error('API response is not an array');
-      console.error('Received HTML:', response.data); // Log the received HTML
+      console.error('Received HTML:', response.data);
       throw new Error('API response is not an array');
     }
   } catch (error) {
